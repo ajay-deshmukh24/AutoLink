1644fcee03dbaf0cb430b609d968bc6768366b76
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-03-16 18:55:30 +0530

    make db update and queue pushing for new trigger atomic

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,13 +1,39 @@
 import express from "express";
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const client = new PrismaClient();
+// console.log(Object.keys(client));
 
 const app = express();
 
 // password logic
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+app.get("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
 
-  // store in db a new trigger
+  // store in db a new trigger and its outbox
+  await client.$transaction(async (tx: Prisma.TransactionClient) => {
+    // console.log(Object.keys(tx));
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        // metadata: body
+      },
+    });
+
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+
   // push it on to a queue (kafka/redis)
+
+  res.json({
+    message: "Webhook received",
+  });
 });
+
+app.listen(3002);
