c7d96b30e7c7908f359af8211cd8cf6af4309f38
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-03-22 11:37:28 +0530

    processor of zapRunOutbox complete

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,16 +1,49 @@
 import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
 
 const client = new PrismaClient();
 
+const TOPIC_NAME = "events";
+
+const kafka = new Kafka({
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
+
 async function main() {
   while (1) {
+    const producer = kafka.producer();
+    await producer.connect();
+
     // doubt--why not client.ZapRunOutbox here
     const pendingRows = await client.zapRunOutbox.findMany({
       where: {},
       take: 10,
     });
 
-    pendingRows.forEach((r) => {});
+    // const pendingRows: {
+    //     id: string;
+    //     zapRunId: string;
+    // }[]
+
+    // push it on to a queue (kafka/redis)
+    await producer.send({
+      topic: TOPIC_NAME,
+      messages: pendingRows.map((r) => {
+        return {
+          value: r.zapRunId,
+        };
+      }),
+    });
+
+    // delete entries from zapRunOutbox
+    await client.zapRunOutbox.deleteMany({
+      where: {
+        id: {
+          in: pendingRows.map((x) => x.id),
+        },
+      },
+    });
   }
 }
 
