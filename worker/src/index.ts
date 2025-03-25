7e6a8bd0dc82ef76c10196e6d1912454131d2bf2
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-03-25 17:01:19 +0530

    worker crash prevention added

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -15,6 +15,8 @@ async function main() {
   await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
 
   await consumer.run({
+    // now we have to acknowledge the kafka about success
+    autoCommit: false,
     eachMessage: async ({ topic, partition, message }) => {
       console.log({
         partition,
@@ -22,7 +24,19 @@ async function main() {
         value: message.value.toString(),
       });
 
-      //   await new Promise(r=>setTimeout(r,1000))
+      await new Promise((r) => setTimeout(r, 5000));
+
+      // console.log("processing done")
+
+      //   NOTE: worker should acknowledge kafka that msg has been processed successfully it should not be considered default after consuming msg
+
+      await consumer.commitOffsets([
+        {
+          topic: TOPIC_NAME,
+          partition: partition,
+          offset: (parseInt(message.offset) + 1).toString(), //5
+        },
+      ]);
     },
   });
 }
