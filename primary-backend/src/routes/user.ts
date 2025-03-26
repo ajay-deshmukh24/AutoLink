c45ca74591d3de0a7f93791c42342ffa69973549
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-03-26 15:55:29 +0530

    work proceed on primary backend

--- primary-backend/src/routes/user.ts
+++ primary-backend/src/routes/user.ts
@@ -1,10 +1,48 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware";
+import { SigninSchema, SignupSchema } from "../types";
+import { prismaClient } from "../db";
 
 const router = Router();
 
-router.post("/signup", (req, res) => {
-  console.log("signup handler");
+router.post("/signup", async (req, res) => {
+  // console.log("signup handler");
+  const body = req.body;
+  const parsedData = SignupSchema.safeParse(body);
+
+  if (!parsedData.success) {
+    console.log(parsedData.error);
+    res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+
+  const userExists = await prismaClient.user.findFirst({
+    where: {
+      email: parsedData.data?.username,
+    },
+  });
+
+  if (userExists) {
+    res.status(403).json({
+      message: "user already exists",
+    });
+  }
+
+  await prismaClient.user.create({
+    data: {
+      email: parsedData.data?.username!,
+      // TODO: Dont store passwords in plaintext, hash it
+      password: parsedData.data?.password!,
+      name: parsedData.data?.name!,
+    },
+  });
+
+  // TODO: send verify email to  user on signup
+
+  res.json({
+    message: "please verify your account by checking your email",
+  });
 });
 
 router.post("/signin", (req, res) => {
