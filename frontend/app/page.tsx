54448f8ece364db784576288738cc957823867e7
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-03-29 16:02:18 +0530

    frontend started with next appbar-completed

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+
+import { Appbar } from "@/components/Appbar";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Appbar></Appbar>
+      {/* <Hero></Hero>
+      <HeroVideo></HeroVideo> */}
+    </main>
+  );
+}
