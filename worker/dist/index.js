3d2bedcac5027536379a92087ec46b485bde8672
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-05-22 17:02:38 +0530

    parser and checks written to parse the metadata

--- worker/dist/index.js
+++ worker/dist/index.js
@@ -10,7 +10,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const kafkajs_1 = require("kafkajs");
-// const client = new PrismaClient();
+const client_1 = require("@prisma/client");
+const prismaClient = new client_1.PrismaClient();
 const TOPIC_NAME = "events";
 const kafka = new kafkajs_1.Kafka({
     clientId: "event-worker",
@@ -21,17 +22,73 @@ function main() {
         const consumer = kafka.consumer({ groupId: "main-worker" });
         yield consumer.connect();
         yield consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
+        // need producer beacuse if not last action of zap we need to push zap again to the queue
+        const producer = kafka.producer();
+        yield producer.connect();
         yield consumer.run({
             autoCommit: false, // now manually we have to acknowledge the kafka about completion
             eachMessage: (_a) => __awaiter(this, [_a], void 0, function* ({ topic, partition, message }) {
+                var _b, _c, _d;
                 // pull the event from kafka queue
                 console.log({
                     partition,
                     offset: message.offset,
                     value: message.value.toString(),
                 });
+                if (!((_b = message.value) === null || _b === void 0 ? void 0 : _b.toString())) {
+                    return;
+                }
+                const parsedValue = JSON.parse((_c = message.value) === null || _c === void 0 ? void 0 : _c.toString());
+                const zapRunId = parsedValue.zapRunId;
+                const stage = parsedValue.stage;
+                // find the associated zap to run from zapRun table
+                const zapRunDetails = yield prismaClient.zapRun.findFirst({
+                    where: {
+                        id: zapRunId,
+                    },
+                    include: {
+                        zap: {
+                            include: {
+                                actions: {
+                                    include: {
+                                        type: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                });
+                // ---------opt-2------------
+                // send query to get the zap id
+                // send query to get back actions associated to this zap id
+                // send query to find available actions
+                const currentAction = zapRunDetails === null || zapRunDetails === void 0 ? void 0 : zapRunDetails.zap.actions.find((x) => x.sortingOrder === stage);
+                if (!currentAction) {
+                    console.log("current action not found");
+                    return;
+                }
+                if (currentAction.type.id === "email") {
+                    console.log("sending mail");
+                }
+                if (currentAction.type.id === "send-sol") {
+                    console.log("sending solana");
+                }
                 // process the current event here
-                yield new Promise((r) => setTimeout(r, 5000));
+                // await new Promise((r) => setTimeout(r, 5000));
+                const lastStage = (((_d = zapRunDetails === null || zapRunDetails === void 0 ? void 0 : zapRunDetails.zap.actions) === null || _d === void 0 ? void 0 : _d.length) || 1) - 1; //1
+                if (lastStage !== stage) {
+                    yield producer.send({
+                        topic: TOPIC_NAME,
+                        messages: [
+                            {
+                                value: JSON.stringify({
+                                    stage: stage + 1,
+                                    zapRunId,
+                                }),
+                            },
+                        ],
+                    });
+                }
                 console.log("processing done");
                 //   NOTE: worker should acknowledge kafka that msg has been processed successfully it should not be considered default after consuming msg
                 yield consumer.commitOffsets([
---
+++ worker/src/parser.ts
@@ -0,0 +1,49 @@
+export function parse(
+  text: string,
+  values: any,
+  startDelimeter = "{",
+  endDelimeter = "}"
+) {
+  // You received {comment.amount} moeny from {comment.link}
+  let startIndex = 0;
+  let endIndex = 0;
+
+  let finalString = "";
+  while (endIndex < text.length) {
+    if (text[startIndex] === startDelimeter) {
+      let startPoint = startIndex + 1;
+      let endPoint = startIndex + 2;
+      while (text[endPoint] !== endDelimeter) {
+        endPoint++;
+      }
+
+      let stringHoldingValue = text.slice(startIndex + 1, endPoint);
+      const keys = stringHoldingValue.split(".");
+      let localValues = {
+        ...values,
+      };
+
+      console.log(keys);
+
+      for (let i = 0; i < keys.length; i++) {
+        if (typeof localValues === "string") {
+          localValues = JSON.parse(localValues);
+        }
+        localValues = localValues[keys[i]];
+      }
+
+      finalString += localValues;
+      startIndex = endPoint + 1;
+      endIndex = endPoint + 1;
+    } else {
+      finalString += text[startIndex];
+      startIndex++;
+      endIndex++;
+    }
+  }
+
+  if (text[startIndex]) {
+    finalString += text[startIndex];
+  }
+  return finalString;
+}
